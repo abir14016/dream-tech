ad2e1dabe4cc33c9c2a7f878c80152213fed1faa
abir14016 <abir14016@gmail.com>
2023-08-25 14:57:15 +0600

    dropdown toggle implement

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -2,14 +2,12 @@ import Image from "next/image";
 import logo from "../../assets/images/dream-tech-logo.png"
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fas, faHome, faMicrochip, faBolt, faMemory, faDisplay, faGears, faHardDrive, faServer } from "@fortawesome/free-solid-svg-icons";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-
-
-
+import { faMicrochip, faBolt, faMemory, faDisplay, faGears, faHardDrive, faServer } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 
 const Navbar = () => {
+    const [menu, setMenu] = useState(false);
     const dropDownItems = (
         <>
             <li>
@@ -74,12 +72,18 @@ const Navbar = () => {
         <div className="navbar bg-neutral sticky top-0 border-b-2 border-secondary z-30 backdrop-blur bg-transparent shadow-lg text-white">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    <label tabIndex={0} className="btn btn-circle btn-primary swap swap-rotate">
+                        <input onClick={() => { setMenu(!menu) }} type="checkbox" />
+                        <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+                        <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52">
-                        {dropDownItems}
-                    </ul>
+                    {
+                        menu && <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-4 shadow bg-base-200 rounded-box w-[100vw] md:w-96">
+                            {dropDownItems}
+                        </ul>
+                    }
+
+
                 </div>
                 <Link href="/">
                     <Image src={logo} width={80} height={80} alt="logo" />
