aac942f3c5ea032f33516911b2c848fd3ea9379a
abir14016 <abir14016@gmail.com>
2023-08-25 14:12:00 +0600

    dropdown menu created

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,9 +1,75 @@
 import Image from "next/image";
 import logo from "../../assets/images/dream-tech-logo.png"
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas, faHome, faMicrochip, faBolt, faMemory, faDisplay, faGears, faHardDrive, faServer } from "@fortawesome/free-solid-svg-icons";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+
+
+
 
 
 const Navbar = () => {
+    const dropDownItems = (
+        <>
+            <li>
+                <Link href="cpu">
+                    <button>
+                        <FontAwesomeIcon className="mr-2 text-secondary" icon={faMicrochip}></FontAwesomeIcon>
+                        <span className="btm-nav-label">CPU / Processor</span>
+                    </button>
+                </Link>
+            </li>
+            <li>
+                <Link href="cpu">
+                    <button>
+                        <FontAwesomeIcon className="mr-2 text-secondary" icon={faServer}></FontAwesomeIcon>
+                        <span className="btm-nav-label">Motherboard</span>
+                    </button>
+                </Link>
+            </li>
+            <li>
+                <Link href="cpu">
+                    <button>
+                        <FontAwesomeIcon className="mr-2 text-secondary" icon={faHardDrive}></FontAwesomeIcon>
+                        <span className="btm-nav-label">Ram</span>
+                    </button>
+                </Link>
+            </li>
+            <li>
+                <Link href="cpu">
+                    <button>
+                        <FontAwesomeIcon className="mr-2 text-secondary" icon={faBolt}></FontAwesomeIcon>
+                        <span className="btm-nav-label">Power Supply Unit</span>
+                    </button>
+                </Link>
+            </li>
+            <li>
+                <Link href="cpu">
+                    <button>
+                        <FontAwesomeIcon className="mr-2 text-secondary" icon={faMemory}></FontAwesomeIcon>
+                        <span className="btm-nav-label">Storage Device</span>
+                    </button>
+                </Link>
+            </li>
+            <li>
+                <Link href="cpu">
+                    <button>
+                        <FontAwesomeIcon className="mr-2 text-secondary" icon={faDisplay}></FontAwesomeIcon>
+                        <span className="btm-nav-label">Monitor</span>
+                    </button>
+                </Link>
+            </li>
+            <li>
+                <Link href="cpu">
+                    <button>
+                        <FontAwesomeIcon className="mr-2 text-secondary" icon={faGears}></FontAwesomeIcon>
+                        <span className="btm-nav-label">Others</span>
+                    </button>
+                </Link>
+            </li>
+        </>
+    );
     return (
         <div className="navbar bg-neutral sticky top-0 border-b-2 border-secondary z-30 backdrop-blur bg-transparent shadow-lg text-white">
             <div className="navbar-start">
@@ -11,10 +77,8 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Homepage</a></li>
-                        <li><a>Portfolio</a></li>
-                        <li><a>About</a></li>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52">
+                        {dropDownItems}
                     </ul>
                 </div>
                 <Link href="/">
@@ -22,7 +86,7 @@ const Navbar = () => {
                 </Link>
             </div>
             <div className="navbar-center">
-
+                {/* no element */}
             </div>
             <div className="navbar-end">
                 <Link href="/pc-builder">
