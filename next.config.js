bac539b022fcc646a9e16369c46706c0b9259612
abir14016 <abir14016@gmail.com>
2023-08-26 23:28:55 +0600

    random featured product section

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["www.trustedreviews.com", "www.asus.com", "www.groovypost.com", "www.knowcomputing.com", "www.freecodecamp.org", "assets-prd.ignimgs.com", "assetsio.reedpopcdn.com"]
+  }
 }
 
 module.exports = nextConfig
--- src/components/ui/FeaturedProducts.js
+++ src/components/ui/FeaturedProducts.js
@@ -1,9 +1,20 @@
+import { useGetRandomProductsQuery } from '@/redux/api/apiSlice';
 import React from 'react';
+import FeaturedProduct from './FeaturedProduct';
 
 const FeaturedProducts = () => {
+    const { data: featuredProducts, isLoading } = useGetRandomProductsQuery();
     return (
         <div className='mt-10 md:mt-16'>
             <h2 className=' uppercase text-xl md:text-3xl text-secondary text-center'>Featured Products</h2>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:px-36 my-5 md:my-4 lg:my-6'>
+                {
+                    featuredProducts?.map((featuredProduct) => <FeaturedProduct
+                        key={featuredProduct._id}
+                        featuredProduct={featuredProduct}
+                    ></FeaturedProduct>)
+                }
+            </div>
         </div>
     );
 };
