cea503aad7fab04a7412ec169e9b3b881c9371b3
abir14016 <abir14016@gmail.com>
2023-08-26 20:52:47 +0600

    provide redux store to the whole aplication

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,16 @@ import Navbar from '@/components/layouts/Navbar'
 import '@/styles/globals.css'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { Provider } from 'react-redux';
+import { store } from '@/redux/store';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </Provider>
   )
 }
