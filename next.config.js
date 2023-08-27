44f4070c7967b3d58bfa79c77ba81f69930d121c
abir14016 <abir14016@gmail.com>
2023-08-27 21:22:48 +0600

    improve linting

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.trustedreviews.com", "www.asus.com", "www.groovypost.com", "www.knowcomputing.com", "www.freecodecamp.org", "assets-prd.ignimgs.com", "assetsio.reedpopcdn.com"]
+    domains: [
+      "www.trustedreviews.com",
+      "www.asus.com",
+      "www.groovypost.com",
+      "www.knowcomputing.com",
+      "www.freecodecamp.org",
+      "assets-prd.ignimgs.com",
+      "assetsio.reedpopcdn.com"
+    ]
   }
 }
 
