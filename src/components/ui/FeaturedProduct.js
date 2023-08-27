4d2d169cd0317cb07814241defa7486b5b1abac4
abir14016 <abir14016@gmail.com>
2023-08-27 22:35:47 +0600

    design fetured product card

--- src/components/ui/FeaturedProduct.js
+++ src/components/ui/FeaturedProduct.js
@@ -1,31 +1,46 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 
 const FeaturedProduct = ({ featuredProduct }) => {
-    const { _id, image, name, category, status, price, individualRating } = featuredProduct
+    const { _id, image, name, category, status, price, individualRating } = featuredProduct;
+    const filledStars = Math.floor(individualRating);
     return (
-        <div class="p-4">
-            <a href="" class="c-card block bg-base-200 shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-                <div class="relative pb-48 overflow-hidden">
-                    <img class="absolute inset-0 h-full w-full object-cover" src={image} alt="" />
+        <div className="p-4">
+            <a href="" className="c-card block bg-base-200 shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+                <div className="relative pb-48 overflow-hidden">
+                    <Image
+                        // style={{ width: "auto", height: "auto" }}
+                        className="absolute inset-0 h-full w-full object-cover"
+                        src={image}
+                        alt="product-image"
+                        layout="fill"
+                        sizes="100vw"
+                    />
                 </div>
-                <div class="p-4">
-                    <span class="inline-block px-2 py-1 leading-none bg-warning text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">{category}</span>
-                    <h2 class="mt-2 mb-2  font-bold">{name}</h2>
-                    <div class="mt-3 flex items-center">
-                        <span class="text-sm font-semibold"></span>&nbsp;<span class="font-bold text-xl">{price}</span>&nbsp;<span class="text-xl font-semibold">$</span>
+                <div className="p-4">
+                    <span className="inline-block px-2 py-1 leading-none bg-warning text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">{category}</span>
+                    <h2 className="mt-2 mb-2  font-bold">{name}</h2>
+                    <div className="mt-3 flex items-center">
+                        <span className="text-sm font-semibold"></span>&nbsp;<span className="font-bold text-xl">{price}</span>&nbsp;<span className="text-xl font-semibold">$</span>
                     </div>
                 </div>
-                <div class="p-4 border-t border-b text-xs text-gray-700">
-                    <span class="flex items-center mb-1">
-                        <i class="far fa-clock fa-fw mr-2 text-gray-900"></i> 3 Tage
-                    </span>
-                    <span class="flex items-center">
-                        <i class="far fa-address-card fa-fw text-gray-900 mr-2"></i> Ermäßigung mit Karte
-                    </span>
+                <div className="p-4 border-t border-b text-xs text-gray-500">
+                    {
+                        status === 'In Stock' ? <p className='text-success'>{status}</p> : <p className='text-error'>{status}</p>
+                    }
+
                 </div>
-                <div class="p-4 flex items-center text-sm text-gray-600"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-gray-400"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><span class="ml-2">34 Bewertungen</span></div>
+                <div className="p-4 flex items-center text-sm text-gray-600">
+                    {Array.from({ length: 5 }, (_, index) => (
+                        <FontAwesomeIcon
+                            key={index}
+                            className={`text-${index < filledStars ? 'secondary' : 'primary'}`}
+                            icon={faStar}
+                        />
+                    ))}
+                </div>
             </a>
         </div>
     );
