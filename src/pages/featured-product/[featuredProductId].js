2e9c08c45240de2e3c7aba0ebddd63d107079554
abir14016 <abir14016@gmail.com>
2023-08-29 20:41:02 +0600

    single product loaded by _id inside featured product detail page

--- src/pages/featured-product/[featuredProductId].js
+++ src/pages/featured-product/[featuredProductId].js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const ProductDetailPage = () => {
+const ProductDetailPage = ({ product }) => {
+    if (!product) {
+        return <p>Loading....</p>
+    }
+    console.log(product);
     return (
         <div>
             <h2>Product detail page</h2>
@@ -9,3 +13,17 @@ const ProductDetailPage = () => {
 };
 
 export default ProductDetailPage;
+
+
+export const getServerSideProps = async (context) => {
+    const { params } = context
+    console.log(params.featuredProductId)
+    const res = await fetch(`http://localhost:5000/product/${params.featuredProductId}`)
+    const data = await res.json()
+
+    return {
+        props: {
+            product: data
+        }
+    }
+}
