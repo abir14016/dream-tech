61c37fddb1c8810206868ba269c8618e6933fec6
abir14016 <abir14016@gmail.com>
2023-08-30 21:58:55 +0600

    resolved dynamic color issue

--- src/components/ui/FeaturedCategory.js
+++ src/components/ui/FeaturedCategory.js
@@ -4,16 +4,32 @@ import React from 'react';
 
 const FeaturedCategory = ({ category }) => {
     const { title, bgColor, icon } = category;
+    let categoryClass = "bg-gray-700"; // Default background color class
+
+    // Apply specific background color classes based on bgColor
+    if (bgColor === "pink-300") {
+        categoryClass = "bg-pink-300";
+    } else if (bgColor === "indigo-300") {
+        categoryClass = "bg-indigo-300";
+    } else if (bgColor === "yellow-300") {
+        categoryClass = "bg-yellow-300";
+    } else if (bgColor === "blue-300") {
+        categoryClass = "bg-blue-300";
+    } else if (bgColor === "green-300") {
+        categoryClass = "bg-green-300";
+    } else if (bgColor === "orange-300") {
+        categoryClass = "bg-orange-300";
+    }
     return (
         <Link href="/" className='md:px-16 lg-px24'>
-            <div className={`bg-${bgColor} shadow-lg rounded-lg h-36  group hover:opacity-70`}>
+            <div className={`${categoryClass} shadow-lg rounded-lg h-36 group hover:opacity-70`}>
                 <div className='flex justify-end'>
                     <div className="badge badge-primary">featured</div>
                 </div>
                 <div className='flex justify-center items-center'>
                     <div className=''>
                         <div className='flex justify-center'>
-                            <FontAwesomeIcon className='text-5xl font-extrabold text-white group-hover:text-secondary' icon={icon} />
+                            <FontAwesomeIcon className='text-5xl font-extrabold text-secondary group-hover:text-white group-hover:animate-spin' icon={icon} />
                         </div>
                         <div>
                             <h2 className='text-center text-lg font-semibold text-gray-600'>{title}</h2>
--- src/pages/featured-product/[featuredProductId].js
+++ src/pages/featured-product/[featuredProductId].js
@@ -16,9 +16,8 @@ export default ProductDetailPage;
 
 
 export const getServerSideProps = async (context) => {
-    const { params } = context
-    console.log(params.featuredProductId)
-    const res = await fetch(`http://localhost:5000/product/${params.featuredProductId}`)
+    const { params } = context;
+    const res = await fetch(`http://localhost:5000/product/${params.featuredProductId}`);
     const data = await res.json()
 
     return {
