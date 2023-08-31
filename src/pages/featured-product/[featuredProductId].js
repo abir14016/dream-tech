afa2a417416b9bd174a1fe678653461f5467953c
abir14016 <abir14016@gmail.com>
2023-08-31 23:56:33 +0600

    product detail page modified

--- src/pages/featured-product/[featuredProductId].js
+++ src/pages/featured-product/[featuredProductId].js
@@ -4,14 +4,14 @@ const ProductDetailPage = ({ product }) => {
     if (!product) {
         return <p>Loading....</p>
     }
-    console.log(product);
+    const { _id, image, name, category, status, price, individualRating } = product;
     return (
         <section class=" body-font overflow-hidden">
             <div class="container px-5 py-24 mx-auto">
                 <div class="lg:w-4/5 mx-auto flex flex-wrap">
                     <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.image} />
                     <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+                        <h2 class="text-sm title-font text-gray-500 tracking-widest">{name}</h2>
                         <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
                         <div class="flex mb-4">
                             <span class="flex items-center">
