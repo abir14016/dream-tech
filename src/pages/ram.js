994365510109f937a92b0f8a2a08876162c27df3
abir14016 <abir14016@gmail.com>
2023-09-03 23:30:48 +0600

    design of ram page

--- src/pages/ram.js
+++ src/pages/ram.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RamPage = () => {
     return (
-        <div>
+        <div className='bg-blue-400'>
             <h2>This is ram page</h2>
         </div>
     );
